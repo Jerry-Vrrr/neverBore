b57bf2855ad599a4d544f15106b4e9e93ddce225
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-20 15:24:37 -0500

    Delete and organize folders

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import '../scss/_App.scss'
 import { rawData } from '../apiCals';
 import React, {Component} from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Swiper from './Swiper';
+import LikeBar from './LikeBar';
+import Filter from './Filter';
 
 class App extends Component {
   constructor () {
@@ -28,6 +30,8 @@ componentDidMount() {
       <div className="App">
         <Header />
         <Swiper activity={this.state.info.activity}/>
+        <LikeBar />
+        <Filter />
         <Footer />
       </div>
     );
