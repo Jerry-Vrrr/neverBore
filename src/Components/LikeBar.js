d90659a1aed55c8c0fba28ab11c43b73b19c105e
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-20 15:21:23 -0500

    Create and style like section and buttons

---
+++ src/Components/LikeBar.js
@@ -0,0 +1,14 @@
+import '../scss/_LikeBar.scss'
+
+const LikeBar = () => {
+  return (
+    <section className="like-bar">
+      <button>Let's Go!</button>
+      <button>Favorite</button>
+      <button>Save For Later</button>
+      <button>No F'n Way!</button>
+    </section>
+  );
+};
+
+export default LikeBar;
