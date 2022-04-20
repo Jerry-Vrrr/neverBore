e24cb72b7a1a503f072bb0b30f5159ccd6389718
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-20 15:20:55 -0500

    Style swiper

--- src/Components/Swiper.js
+++ src/Components/Swiper.js
@@ -1,13 +1,10 @@
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import './_Swiper.scss'
+import '../scss/_Swiper.scss'
 
 export default ({activity}) => {
   return (
