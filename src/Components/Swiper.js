14f5c65e190a81bd2f0887a2ac585bbabfb7b44f
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-20 12:17:12 -0500

    Add Swiper and display activity

---
+++ src/Components/Swiper.js
@@ -0,0 +1,33 @@
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import './_Swiper.scss'
+
+export default ({activity}) => {
+  return (
+    <Swiper
+      // install Swiper modules
+      className='swiper'
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide>{<h1>{activity}</h1>}</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
+  );
+};
