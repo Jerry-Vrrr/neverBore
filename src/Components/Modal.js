26040808c609759597a6b582874ae9417774402f
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-21 12:43:41 -0500

    Create Modal

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import raven from '../raven.png'
 
-export default function Modal({open, children, onClose, props}) {
+export default function Modal({open, children, onClose, props,}) {
     if (!open) return null
 
   return (
@@ -9,8 +9,23 @@ export default function Modal({open, children, onClose, props}) {
         <div className='overlay'>
         <div className='modal'>
              {children}
-             <img src={raven}></img>
-            <button onClick={onClose}></button>
+             <div className='modal-head'>
+                <img src={raven}></img>
+                <button onClick={onClose}>&times;</button>
+             </div>
+             <div>
+                 <h1>
+                    {props.education}
+                    
+                 </h1>
+                 
+             </div>
+             <div className='modal-foot'>
+             <a target='blank' href={`https://www.youtube.com/results?search_query=${props.education}`}
+             >Check Out Some Tutorials</a>
+             <button>Add To List</button>
+             <button>View List</button>
+             </div>
         </div>
         </div>
     </>  
