50726cc910da4410714dca39686e46015056b3fa
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-20 23:50:06 -0500

    Set media queries for filter buttons

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,13 +1,12 @@
 
-import '../scss/_Filter.scss'
-
-
 const Filter = () => {
   
   return (
     <section className="filter">
-      
-      
+      <button>Educational</button>
+      <button>Volunteer</button>
+      <button>Relaxation</button>
+      <button>Chores</button>
     </section>
   );
 };
