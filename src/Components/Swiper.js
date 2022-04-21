6c7758a835b7ab864ce5c7f9d81ccc1b167725b6
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-21 12:46:43 -0500

    Update css styling

--- src/Components/Swiper.js
+++ src/Components/Swiper.js
@@ -1,17 +1,17 @@
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import raven from "../raven.png"
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import raven from "../raven.png";
 
-export default ({props}) => {
-  console.log(props)
+export default ({ props, openModal }) => {
+  console.log(props);
   return (
     <Swiper
       // install Swiper modules
-      className='swiper'
+      className="swiper"
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={50}
       slidesPerView={1}
@@ -19,24 +19,42 @@ export default ({props}) => {
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
       onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
+      onSlideChange={() => console.log("slide change")}
     >
       <SwiperSlide>
-      <h2>Educational</h2>
-        <h1><img className='raven'src={raven}/>{props.education}</h1>
-        
+        <h2>Educational</h2>
+        <div>
+          <h1>
+            <img className="raven" src={raven} />
+            <br></br>
+            {props.education}
+            <button onClick={openModal}>expand</button>
+          </h1>
+        </div>
       </SwiperSlide>
       <SwiperSlide>
-      <h2>Volunteer</h2>
-        <h1><img className='raven'src={raven}/><br></br>{props.charity}</h1>
+        <h2>Volunteer</h2>
+        <h1>
+          <img className="raven" src={raven} />
+          <br></br>
+          {props.charity}
+        </h1>
       </SwiperSlide>
       <SwiperSlide>
-      <h2>Relaxation</h2>
-        <h1><img className='raven'src={raven}/><br></br>{props.relaxation}</h1>
+        <h2>Relaxation</h2>
+        <h1>
+          <img className="raven" src={raven} />
+          <br></br>
+          {props.relaxation}
+        </h1>
       </SwiperSlide>
       <SwiperSlide>
-      <h2>Chores</h2>
-      <h1><img className='raven'src={raven}/><br></br>{props.busywork}</h1>
+        <h2>Chores</h2>
+        <h1>
+          <img className="raven" src={raven} />
+          <br></br>
+          {props.busywork}
+        </h1>
       </SwiperSlide>
       ...
     </Swiper>
