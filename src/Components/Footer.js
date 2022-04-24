7363dd97e8bf6e9fefc705b5d07b88cec8a9a76f
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-23 19:42:56 -0500

    Adjust data structure and naming

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,16 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <section className="footer">
-      <article>
-        <a href="https://www.linkedin.com/in/jerry-vohrer-974a97205/">
-          About Site
-        </a>
-      </article>
-      <article>
-        <a href="https://www.linkedin.com/in/jerry-vohrer-974a97205/">
-          Jerry Voher
-        </a>
-      </article>
+      
     </section>
   );
 };
